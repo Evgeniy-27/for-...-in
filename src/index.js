27535ebcb908602094cc05ef629aab5a715311e9
Evgeniy-27 <Li.Evgen@yandex.ru>
2020-07-13 22:05:26 +0700

    Доработано

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ export const obj = {
   name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
 };
 
-export function orderByProps(obj, [key1, key2]) {
+export function orderByProps(obj, [...rest]) {
   const result = [];
-  result.push({ key: key1, value: obj[key1] },
-              { key: key2, value: obj[key2] });
+  rest.forEach(elem => {
+    result.push({ 'key': elem, 'value': obj[elem] });
+  })
 
   const arrSort = [];
   Object.keys(obj).forEach((elem) => {
-    if (elem !== key1 && elem !== key2) {
+    if (rest.includes(elem) !== true) {
       arrSort.push(elem);
     }
   });
